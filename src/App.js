f6d6ff9e106ab021b0adfb0872e51c1bde9d9c99
luisfff29 <luisfff29@gmail.com>
2020-06-24 00:59:15 -0500

    display fields from API into React page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { posts: [] };
+  }
+  componentDidMount() {
+    fetch("http://localhost:8000/api/posts/")
+      .then((res) => res.json())
+      .then((data) => this.setState({ posts: data }));
+  }
+  render() {
+    return (
+      <div style={{ margin: "2em" }}>
+        {this.state.posts.map((post) => {
+          return (
+            <>
+              <h1>{post.boast_or_roast}</h1>
+              <ul>
+                <li>{post.url}</li>
+                <li>{post.text}</li>
+                <li>{post.up_vote}</li>
+                <li>{post.down_vote}</li>
+                <li>{post.date}</li>
+                <li>{post.total_votes}</li>
+              </ul>
+            </>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
